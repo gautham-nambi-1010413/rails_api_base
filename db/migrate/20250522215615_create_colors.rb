c93b9eed6af9327d3e9fb98e7bcee867b74b648a
Gautham Nambi <gautham.nambi@freshworks.com>
2025-05-26 05:52:49 +0530

    migrations and spec updated

--- db/migrate/20250522215615_create_colors.rb
+++ db/migrate/20250522215615_create_colors.rb
@@ -1,8 +1,8 @@
 class CreateColors < ActiveRecord::Migration[8.0]
   def change
-    create_table :colors do |t|
+    create_table :colors, if_not_exists: true do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :color_code
+      t.string :color_code, null: false
 
       t.timestamps
     end
