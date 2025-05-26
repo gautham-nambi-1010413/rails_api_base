d99c4d890e6e150daad4e4f02ba61cb7fe900f78
Gautham Nambi <gautham.nambi@freshworks.com>
2025-05-26 05:54:34 +0530

    removed spec

--- spec/decorators/api/v1/color_decorator_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe API::V1::ColorDecorator do
-end
--- spec/factories/colors.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :color do
-    association :user
-    color_code { '#FF0000' }
-  end
-end
