50ad517103e94a3267ed1a5ed5afde2bc73c4838
Gautham Nambi <gautham.nambi@freshworks.com>
2025-05-27 04:31:25 +0530

    doing rest call

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -7,7 +7,17 @@ module API
       skip_after_action :verify_authorized
 
       def get_delayed_jobs
-        render json: { count: Delayed::Job.count }
+        begin
+          response = HTTParty.get('https://api.restful-api.dev/objects')
+
+          if response.code == 200
+            render json: { count: Delayed::Job.count }
+          else
+            render json: { error: 'External API request failed' }, status: :bad_request
+          end
+        rescue StandardError => e
+          render json: { error: e.message }, status: :bad_request
+        end
       end
 
       def status
