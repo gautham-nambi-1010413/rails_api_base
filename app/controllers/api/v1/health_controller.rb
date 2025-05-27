68e9a6aeee73a654a90729b4488bece56608efa8
Gautham Nambi <gautham.nambi@freshworks.com>
2025-05-27 13:21:21 +0530

    rest call testing

--- app/controllers/api/v1/health_controller.rb
+++ app/controllers/api/v1/health_controller.rb
@@ -13,6 +13,26 @@ module API
       def status
         render json: { online: true }
       end
+
+      def rest
+        begin
+          response = HTTParty.get('https://api.restful-api.dev/objects')
+
+          if response.code == 200
+            response_data = JSON.parse(response.body)
+
+            if response_data.all? { |item| item.key?('id') && item.key?('name') && item.key?('data') }
+              render json: response_data
+            else
+              render json: { error: 'Invalid response format' }, status: :bad_request
+            end
+          else
+            render json: { error: 'External API request failed' }, status: :bad_request
+          end
+        rescue StandardError => e
+          render json: { error: e.message }, status: :bad_request
+        end
+      end
     end
   end
 end
